a8e12adc800ef06eabcdc8b45e4dd5c166a57329
Rishabh Gour <rishabhgour62@gmail.com>
2017-12-08 11:58:12 +0530

    code completed

--- emitter.js
+++ emitter.js
@@ -8,5 +8,9 @@ Emitter.prototype.on = function(event_type, event_listener){
 }
 
 Emitter.prototype.emit = function(event_type){
-    
+    if(this.events[event_type]){
+        this.events[event_type].forEach(function(event_listener) {
+            event_listener();
+        }, this);
+    }
 }
